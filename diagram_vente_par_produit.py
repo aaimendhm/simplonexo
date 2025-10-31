deacba9b40bb36b0c9705fc3c3b2b93e17d55157
aaimendhm <dahmaniaimene07@gmail.com>
2025-10-31 12:32:58 +0100

    Update diagram_vente_par_produit.py

--- diagram_vente_par_produit.py
+++ diagram_vente_par_produit.py
@@ -1,6 +1,10 @@
-import sqlite3
+import plotly.express as px
+import pandas as pd
 
-conn = sqlite3.connect('ventes.csv')
-cur = conn.cursor()
-cur.execute("SELECT c2; SUM (c3 * c4) AS ventes_parproduits; FROM ventes; GROUP BY c2")
-conn.close()
+données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
+
+figure = px.pie(données, values='qte', names='produit', title='quantité vendue par produits')
+
+figure.write_html('ventes-par-produits.html')
+
+print('ventes-par-produits.html généré avec succès !')
