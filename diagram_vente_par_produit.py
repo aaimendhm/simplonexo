080eee67fca45ad605fe7fb5ca4e9ffaa35a4292
aaimendhm <dahmaniaimene07@gmail.com>
2025-10-31 12:26:47 +0100

    Update diagram_vente_par_produit.py

--- diagram_vente_par_produit.py
+++ diagram_vente_par_produit.py
@@ -1,2 +1,6 @@
-import matplotlib.pyplot as sp
+import sqlite3
 
+conn = sqlite3.connect('ventes.csv')
+cur = conn.cursor()
+cur.execute("SELECT c2; SUM (c3 * c4) AS ventes_parproduits; FROM ventes; GROUP BY c2")
+conn.close()
