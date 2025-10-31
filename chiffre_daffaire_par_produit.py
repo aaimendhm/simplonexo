a0a1d8c1bda5fa7f321138c8abac195481b5a442
aaimendhm <dahmaniaimene07@gmail.com>
2025-10-31 12:40:40 +0100

    Update chiffre_daffaire_par_produit.py

--- chiffre_daffaire_par_produit.py
+++ chiffre_daffaire_par_produit.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-figure = px.pie(données, values='qte'*'prix', names='produit', title='chiffre d affaire par produits')
+figure = px.pie(données, values='qte * prix', names='produit', title='chiffre d affaire par produits')
 
 figure.write_html('chiffre-d-affaire-par-produits.html')
 
